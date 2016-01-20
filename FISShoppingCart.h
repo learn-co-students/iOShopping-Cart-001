9ddca106ba83ba5581b41e6faa9d4b9aeb368d83
joshmotley <joshkmotley@gmail.com>
2016-01-20 16:41:35 -0500

    complete tasks

--- FISShoppingCart.h
+++ FISShoppingCart.h
@@ -7,7 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
+#import  "FISItem.h"
 
 @interface FISShoppingCart : NSObject
+
+@property (nonatomic) NSMutableArray *items;
+
+-(NSUInteger)calculateTotalPriceInCents;
+-(void)addItem:(FISItem *)item;
+-(void)removeItem:(FISItem *)item;
+-(void)removeAllItemsLikeItem:(FISItem *)item;
+-(void)sortItemsByNameAsc;
+-(void)sortItemsByNameDesc;
+-(void)sortItemsByPriceInCentsAsc;
+-(void)sortItemsByPriceInCentsDesc;
+-(NSArray *)allItemsWithName:(NSString *)name;
+-(NSArray *)allItemsWithMinimumPriceInCents:(NSUInteger)cents;
+-(NSArray *)allItemsWithMaximumPriceInCents:(NSUInteger)cents;
+
 
 @end
--- FISShoppingCart.m
+++ FISShoppingCart.m
@@ -10,4 +10,98 @@
 
 @implementation FISShoppingCart
 
+-(NSUInteger)calculateTotalPriceInCents{
+    NSUInteger result=0;
+    for (FISItem *item in self.items) {
+        result+=item.priceInCents;
+    }
+
+    
+    return result;
+};
+-(void)addItem:(FISItem *)item{
+    
+    [self.items addObject:item];
+
+};
+-(void)removeItem:(FISItem *)item{
+    
+    [self.items removeObjectAtIndex:[self.items indexOfObject:item]];
+    
+};
+-(void)removeAllItemsLikeItem:(FISItem *)item{
+    
+
+            [self.items removeObject:item];
+
+    
+};
+-(void)sortItemsByNameAsc{
+    
+    NSSortDescriptor *sort = [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES];
+    NSArray *sortArray = [NSArray arrayWithObject:sort];
+    [self.items sortUsingDescriptors:sortArray];
+    
+};
+-(void)sortItemsByNameDesc{
+   
+    NSSortDescriptor *sort = [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:NO];
+    NSArray *sortArray = [NSArray arrayWithObject:sort];
+    [self.items sortUsingDescriptors:sortArray];
+    
+};
+-(void)sortItemsByPriceInCentsAsc{
+    
+    NSSortDescriptor *sort = [NSSortDescriptor sortDescriptorWithKey:@"priceInCents" ascending:YES];
+    NSArray *sortArray = [NSArray arrayWithObject:sort];
+    [self.items sortUsingDescriptors:sortArray];
+    
+};
+-(void)sortItemsByPriceInCentsDesc{
+    
+    NSSortDescriptor *sort = [NSSortDescriptor sortDescriptorWithKey:@"priceInCents" ascending:NO];
+    NSArray *sortArray = [NSArray arrayWithObject:sort];
+    [self.items sortUsingDescriptors:sortArray];
+    
+};
+-(NSArray *)allItemsWithName:(NSString *)name{
+   
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    for (FISItem *item in self.items) {
+
+    if ([item.name isEqualToString:name] ) {
+        [result addObject:item];
+    }
+        
+    }
+    
+    return result;
+};
+-(NSArray *)allItemsWithMinimumPriceInCents:(NSUInteger)cents{
+    
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    for (FISItem *item in self.items) {
+        
+        if (item.priceInCents >= cents){
+            [result addObject:item];
+        }
+        
+    }
+    
+    return result;
+};
+-(NSArray *)allItemsWithMaximumPriceInCents:(NSUInteger)cents{
+    
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    for (FISItem *item in self.items) {
+        
+        if (item.priceInCents <= cents){
+            [result addObject:item];
+        }
+        
+    }
+    
+    return result;
+};
+
 @end
