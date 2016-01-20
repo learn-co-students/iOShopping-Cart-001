a6cb1846c712fa4b400b99f36aa872baee103f22
joshmotley <joshkmotley@gmail.com>
2016-01-20 16:39:49 -0500

    write methods for custom class to perform on its own properties

---
+++ FISShoppingCart.h
@@ -0,0 +1,13 @@
+//
+//  FISShoppingCart.h
+//  iOShoppingCart
+//
+//  Created by Joshua Motley on 1/20/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISShoppingCart : NSObject
+
+@end
---
+++ FISShoppingCart.m
@@ -0,0 +1,13 @@
+//
+//  FISShoppingCart.m
+//  iOShoppingCart
+//
+//  Created by Joshua Motley on 1/20/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import "FISShoppingCart.h"
+
+@implementation FISShoppingCart
+
+@end
